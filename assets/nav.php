deef3e7a51ea43bcc133179ee53126155b5ff7e4
Stefan Ungureanu <stefan.ungureanu@etu.unistra.fr>
2020-02-18 11:57:18 +0100

    premiere version

---
+++ assets/nav.php
@@ -0,0 +1,7 @@
+<ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Événements</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">EN</a></li>
+        <li><a href="#">FR</a></li>
+</ul>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="AEE - annonces pour étudiants étrangers">
+    <meta name="author" content="Stefan UNGUREANU">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/annonce.css">
+    <title>AEE</title>
+</head>
+
+<body>
+    <h1>AEE</h1>
+    <?php include('assets/nav.php'); ?>
+    <?php include('assets/footer.php'); ?>
+</body>
+</html>
