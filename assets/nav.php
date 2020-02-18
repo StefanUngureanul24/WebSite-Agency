392ec8fda5a2d83d7d93e32e06771363ff05e06d
UNGUREANU STEFAN <stefan.ungureanu@etu.unistra.fr>
2020-02-18 19:18:49 +0100

    Update assets/nav.php

--- assets/nav.php
+++ assets/nav.php
@@ -1,5 +1,6 @@
 <ul>
         <li><a href="#">Accueil</a></li>
+        <li><a href="#">Renseignements</a></li>
         <li><a href="#">Événements</a></li>
         <li><a href="#">Contact</a></li>
         <li><a href="#">EN</a></li>
